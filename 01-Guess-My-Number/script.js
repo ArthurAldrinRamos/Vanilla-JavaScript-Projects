fd1217225bad7907a37b70cc0888822ad13f78c9
Arthur Aldrin Ramos <arthuraldrinramos@gmail.com>
2022-12-15 02:10:41 +0800

    Personal Project: Final

--- 01-Guess-My-Number/script.js
+++ 01-Guess-My-Number/script.js
@@ -8,54 +8,88 @@
 // document.querySelector(`.guess`).value = 11;
 // console.log(document.querySelector(`.guess`).value);
 
-const secretNumber = Math.trunc(Math.random()*20) + 1;
-document.querySelector(`.number`).textContent = secretNumber;
+// ----------------------------------------------------- //
 
+//Global Variables
+let secretNumber = Math.trunc(Math.random()*20) + 1;
+console.log(secretNumber)
 let score = 20;
+let highScore = 0;
 
+//Functions used in the click event..
+const displayMessage = (message) => {
+    document.querySelector(`.message`).textContent = message;
+}
+const yourScore = () => {
+    document.querySelector(`.score`).textContent = score;
+}
+const yourColor = (color) => {
+    document.querySelector(`body`).style.backgroundColor = 
+            color;
+}
+const boxSize = () => {
+    document.querySelector(`.number`).style.width = `30rem`;
+}
 
+//Click Event
 document.querySelector(`.check`).addEventListener(`click`, () => {
     const guess = Number(document.querySelector(`.guess`).value);
     console.log(guess);
 
+    //When no input or answers zero = LOSE
     if(!guess) {
         if (score > 0){
-            document.querySelector(`.message`).textContent = `No number!`
+            displayMessage(`No number!`)
             score--
-            document.querySelector(`.score`).textContent = score;
+            yourScore();
+            yourColor(`red`);
+            boxSize();
         } else {
-            document.querySelector(`.message`).textContent = `You Lose!`
+            displayMessage(`You Lose!`);
         }
 
+    //When player guess is correct = WIN
     } else if(guess === secretNumber){
 
-        if(score < 30){
-        document.querySelector(`.message`).textContent = `You are correct!`;
-        score++;
-        document.querySelector(`.score`).textContent = score;
-        } else {
-        document.querySelector(`.message`).textContent = `You Won!`
-        }
-    } else if(guess > secretNumber) {
+        displayMessage(`You are correct!`);
+        yourColor(`#60b347`);
+        boxSize();
 
-        if (score > 0){
-        document.querySelector(`.message`).textContent = `Too high!`
+        document.querySelector(`.number`).textContent = secretNumber;
+
+        if(score > highScore){
+            highScore = score
+            document.querySelector(`.highscore`).textContent = highScore
+        }
+    
+    //When player guess is wrong = LOSE
+    } else if(guess !== secretNumber) {
+
+        displayMessage(guess > secretNumber ? `Too high!` : `Too low!`);
         score--
-        document.querySelector(`.score`).textContent = score;
-        } else {
-        document.querySelector(`.message`).textContent = `You Lose!`
-        }
+        yourScore();
+        yourColor(`red`);
+        boxSize();
 
-    } else if(guess < secretNumber) {
-
-        if (score > 0){
-        document.querySelector(`.message`).textContent = `Too low!`
-        score--
-        document.querySelector(`.score`).textContent = score;
-        } else {
-        document.querySelector(`.message`).textContent = `You Lose!`
         }
-    }
         
 })
+
+//Again Button - Repeats a round without removing the Highscore
+document.querySelector(`.again`).addEventListener(`click`, () =>{
+
+    score = 20;
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    console.log(secretNumber)
+
+    displayMessage(`Start guessing...!`); 
+    yourScore();
+    document.querySelector(`.number`).textContent = `?`;
+    document.querySelector(`.number`).style.width = `15rem`;
+    document.querySelector(`body`).style.backgroundColor = 
+    `#222`;
+    document.querySelector(`.guess`).value = ``;
+})
+
+
 
