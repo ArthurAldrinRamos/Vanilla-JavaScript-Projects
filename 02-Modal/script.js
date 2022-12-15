22c518c43faba0363e48cc00ac6d374b0ce1349d
Arthur Aldrin Ramos <arthuraldrinramos@gmail.com>
2022-12-15 16:04:25 +0800

    Project Project No. 2: Final

---
+++ 02-Modal/script.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const modal = document.querySelector(`.modal`);
+const overLay = document.querySelector(`.overlay`);
+const btnCloseModal = document.querySelector(`.close-modal`);
+const btnsOpenModal = document.querySelectorAll(`.show-modal`);
+
+const openModal = () => {
+  console.log(`Button Clicked!`);
+  modal.classList.remove(`hidden`);
+  overLay.classList.remove(`hidden`);
+};
+
+const closeModal = () => {
+  modal.classList.add(`hidden`);
+  overLay.classList.add(`hidden`);
+};
+
+console.log(btnsOpenModal);
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener(`click`, openModal);
+}
+
+btnCloseModal.addEventListener(`click`, closeModal);
+overLay.addEventListener(`click`, closeModal);
+document.addEventListener(`keydown`, e => {
+  if (e.key === `Escape` && !modal.classList.contains(`hidden`)) {
+    closeModal();
+  }
+});
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
+ARTHUR ALDRIN RAMOS
 # JavaScript-Projects
+==============================
+
+1. Guess My Number
+-Usage of an event listener
+-Refactored some of the codes
+
+2. Modal
+-Added overlays
+-Added ESC keypress event
+
+
+
+
+
+
+
+
